a8f7a85939e20c48b120ab2bbe21c73217500cdd
Bong <bongster228@github.com>
2021-03-04 20:38:28 +0000

    v1 deploy

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@
 
 mvn -f server/pom.xml package -DskipTests
 echo "maven build complete"
-docker-compose build --no-cache backend nginx
+docker-compose build --no-cache
 echo "docker-compose build compelte"
 docker-compose up --force-recreat

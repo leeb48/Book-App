50cae5e98d8670acb9a66c8315deb3cbde54c535
Bong Lee <bongsoolee91@gmail.com>
2021-03-17 11:01:15 -0700

    fixed a typo in build.sh

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ mvn -f server/pom.xml package -DskipTests
 echo "maven build complete"
 docker-compose build --no-cache
 echo "docker-compose build compelte"
-docker-compose up --force-recreat
+docker-compose up --force-recreate
